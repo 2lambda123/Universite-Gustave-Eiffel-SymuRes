f1f2c9a61396d0fcb900bd55867263f3dcdf5305
Restyled.io <commits@restyled.io>
2024-02-20 02:30:48 +0000

    Restyled by astyle

--- SymuRes/Utilityfunctions/arrayextension.m
+++ SymuRes/Utilityfunctions/arrayextension.m
@@ -1,29 +1,32 @@
 function extarray = arrayextension(array,Next,type)
-% extarray = listextension(array,Next,type)
-% Extend a 2d matrix, cell or array along one of its dimensions (columns or
-% rows) by repeating its content until the corresponding dimension has the
-% required size Next
-%
-% INPUTS
-%---- array : 1d or 2d matrix, cell or array
-%---- Next  : integer, desired size for the chosen dimension
-%---- type  : string, 'column' or 'row', chosen dimension to extend
-%
-% OUTPUTS
-%---- extarray : extended matrix, cell or array
+                    % extarray = listextension(array,Next,type)
+                                 % Extend a 2d matrix, cell or array along one of its dimensions (columns or
+                                         % rows) by repeating its content until the corresponding dimension has the
+                                 % required size Next
+                                 %
+                                 % INPUTS
+                                 %---- array : 1d or 2d matrix, cell or array
+                                 %---- Next  :
+                                 integer, desired size for the chosen dimension
+                                 %---- type  :
+                                 string, 'column' or 'row', chosen dimension to extend
+                                 %
+                                 % OUTPUTS
+                                 %---- extarray :
+                                 extended matrix, cell or array
 
-if strcmp(type,'column')
-    extarray = array;
-    while size(extarray,2) < Next
-        extarray = [extarray array];
-    end
-    extarray = extarray(:,1:Next);
+                                 if strcmp(type,'column')
+                                         extarray = array;
+while size(extarray,2) < Next
+    extarray = [extarray array];
+end
+extarray = extarray(:,1:Next);
 elseif strcmp(type,'row')
-    extarray = array;
-    while size(extarray,1) < Next
-        extarray = [extarray; array];
-    end
-    extarray = extarray(1:Next,:);
+extarray = array;
+while size(extarray,1) < Next
+    extarray = [extarray; array];
+end
+extarray = extarray(1:Next,:);
 else
     warning('Bad extension type, must be one of the following: column, row')
     extarray = [];
